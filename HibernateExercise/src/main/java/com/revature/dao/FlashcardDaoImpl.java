2b9470b48c4f291cbe3c0504bcaa495c19525d7a
iramin1234 <iramin1234@gmail.com>
2018-09-03 12:28:51 -0400

    Dao implementations

---
+++ HibernateExercise/src/main/java/com/revature/dao/FlashcardDaoImpl.java
@@ -0,0 +1,143 @@
+package com.revature.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.revature.beans.Category;
+import com.revature.beans.Flashcard;
+import com.revature.util.HibernateUtil;
+
+public class FlashcardDaoImpl implements FlashcardDao {
+	
+	private SessionFactory sf = HibernateUtil.getSessionFactory();
+
+	public FlashcardDaoImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Flashcard> getFlashcards() {
+		Session s = sf.openSession();
+		List<Flashcard> fl = null;
+		
+		try {
+			fl = s.createQuery("from Flashcard").list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		sf.close();
+		
+		return fl;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Flashcard> getActiveFlashcards() {
+		Session s = sf.openSession();
+		List<Flashcard> fl = null;
+		
+		try {
+			Query q = s.createQuery("from Flashcard where category = :categoryVar"); 
+			q.setParameter("categoryVar", s.createQuery("from Category where isActive = ACTIVE"));
+			fl = q.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		sf.close();
+		
+		return fl;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Flashcard> filterCardsByQuestion(String text) {
+		Session s = sf.openSession();
+		Criteria cr = s.createCriteria(Flashcard.class);
+		cr.add(Restrictions.like("question", text));
+		List<Flashcard> fl = null;
+		
+		try {
+			fl = cr.list();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		sf.close();
+		
+		return fl;
+	}
+
+	@Override
+	public Flashcard getFlashcardById(int id) {
+		Session s = sf.openSession();
+		Flashcard f = null;
+		
+		try {
+			f = (Flashcard) s.get(Flashcard.class, id);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		sf.close();
+		return f;
+	}
+
+	@Override
+	public void addFlashcard(Flashcard f) {
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		try {
+			s.save(f);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		}
+
+		sf.close();
+		
+
+	}
+
+	@Override
+	public void updateFlashcard(Flashcard f) {
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		try {
+			s.merge(f);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		}
+
+		sf.close();
+
+	}
+
+	@Override
+	public void deleteFlashcard(Flashcard f) {
+		Session s = sf.openSession();
+		Transaction tx = s.beginTransaction();
+		try {
+			s.delete(f);
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		}
+
+		sf.close();
+
+	}
+
+}
